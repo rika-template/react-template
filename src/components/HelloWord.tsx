d055fa70c7c823792315aa3e3ead56481bc534c1
Rika-L <2209393334@qq.com>
2024-09-17 13:25:21 +0800

    feat: add HelloWord

---
+++ src/components/HelloWord.tsx
@@ -0,0 +1,5 @@
+function HelloWorld() {
+  return <h1>Rika-React-Template</h1>
+}
+
+export default HelloWorld
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import HelloWorld from '@/components/HelloWord'
+
 function Home() {
-  return <div>Welcome to Rika React Template</div>
+  return <div><HelloWorld /></div>
 }
 
 export default Home
