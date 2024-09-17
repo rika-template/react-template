23e663c0f15270a89e9a5970cf6308162f5685c2
Rika-L <2209393334@qq.com>
2024-09-17 15:48:08 +0800

    feat: edit hello word

--- src/components/HelloWord.tsx
+++ src/components/HelloWord.tsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from 'react'
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 function HelloWorld() {
-  return <h1><div className="icon-[mdi-light--home] size-20"></div></h1>
+  const [randomColor, setRandomColor] = useState('rgb(0, 0, 0)')
+  useEffect(() => {
+    const timeout: NodeJS.Timeout = setInterval(() => {
+      setRandomColor(`rgb(${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)})`)
+    }, 1000)
+    return () => clearInterval(timeout)
+  }, [])
+  return (
+    <div className="flex justify-center mt-[35vh]">
+      <div>
+        <div className="text-5xl font-thin transition-colors duration-1000 ease-linear" style={{ color: randomColor }}>
+          Rika-React-Template
+        </div>
+        <div className="my-4 flex justify-center gap-5 text-gray-500">
+          <a
+            target="_blank"
+            href="https://rika-me.netlify.app/#/"
+            className="icon-[mdi--user] text-5xl hover:text-gray-700"
+            rel="noopener noreferrer"
+          />
+          <a
+            target="_blank"
+            href="https://github.com/rika-template/vue-template"
+            className="icon-[mdi--github] text-5xl hover:text-gray-700"
+            rel="noopener noreferrer"
+          />
+          <a
+            target="_blank"
+            href="https://gitee.com/rika-template/vue-template"
+            className="icon-[simple-icons--gitee] text-5xl hover:text-gray-700"
+            rel="noopener noreferrer"
+          />
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 export default HelloWorld
